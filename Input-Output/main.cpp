c0d20b34ae442807d2a5828de0f3985a7330845a
Sebastián <sebitasalguero@gmail.com>
2022-04-22 18:10:01 -0500

    Entrada y salida básica

---
+++ Input-Output/main.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int int_primero;
+    int int_segundo;
+    int int_tercero;
+    cin >> int_primero >> int_segundo >> int_tercero;
+    cout << int_primero + int_segundo + int_tercero;
+    return 0;
+}
